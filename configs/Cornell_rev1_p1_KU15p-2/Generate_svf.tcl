91c2bcc5292262a9b0032f975932ab761e5c46ad
Dan Gastler <dgastler@gmail.com>
2021-01-22 09:29:24 -0500

    Testing each config

--- configs/Cornell_rev1_p1_KU15p-2/Generate_svf.tcl
+++ configs/Cornell_rev1_p1_KU15p-2/Generate_svf.tcl
@@ -29,8 +29,8 @@ set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 #add the virtex to the chain
 create_hw_device -part xcvu7p-flvb2104-1-e
 
-program_hw_devices -force -svf_file ${apollo_root_path}bit/top_${build_name}.svf ${DEVICE}
+program_hw_devices -force -svf_file ${apollo_root_path}/bit/top_${build_name}.svf ${DEVICE}
 
-write_cfgmem -force -loadbit "up 0 ${apollo_root_path}bit/top_${build_name}.bit" -format mcs -size 128 -file "${apollo_root_path}bit/top_${build_name}.mcs"
+write_cfgmem -force -loadbit "up 0 ${apollo_root_path}/bit/top_${build_name}.bit" -format mcs -size 128 -file "${apollo_root_path}/bit/top_${build_name}.mcs"
 
 delete_hw_target -quiet [get_hw_targets -regexp .*/${SVF_TARGET}]
--- configs/EMP_Cornell_rev1_p2_VU7p-1/src/createC2CSlaveInterconnect.tcl
+++ configs/EMP_Cornell_rev1_p2_VU7p-1/src/createC2CSlaveInterconnect.tcl
@@ -131,22 +131,27 @@ connect_bd_net [get_bd_ports ${AXI_MASTER_CLK}] [get_bd_pins ${C2C}/m_axi_lite_a
 
 endgroup
 
+#================================================================================
+#  Create JTAG AXI Master
+#================================================================================
+set JTAG_AXI_MASTER JTAG_AXI_Master
+[BUILD_JTAG_AXI_MASTER ${JTAG_AXI_MASTER} ${AXI_MASTER_CLK} ${AXI_MASTER_RSTN}]
 
 #================================================================================
 #  Connect C2C master port to interconnect slave port
 #================================================================================
-set mAXI [list ${C2C}/m_axi ${C2C}/m_axi_lite ]
-set mCLK [list ${AXI_MASTER_CLK} ${AXI_MASTER_CLK} ]
-set mRST [list $AXI_MASTER_RSTN $AXI_MASTER_RSTN] 
+set mAXI [list ${C2C}/m_axi ${C2C}/m_axi_lite ${JTAG_AXI_MASTER}/M_AXI]
+set mCLK [list ${AXI_MASTER_CLK}  ${AXI_MASTER_CLK}  ${AXI_MASTER_CLK} ]
+set mRST [list ${AXI_MASTER_RSTN} ${AXI_MASTER_RSTN} ${AXI_MASTER_RSTN}] 
 [BUILD_AXI_INTERCONNECT $AXI_INTERCONNECT_NAME ${AXI_MASTER_CLK} $AXI_MASTER_RSTN $mAXI $mCLK $mRST]
-[AXI_DEV_CONNECT ${C2C_PHY} ${AXI_INTERCONNECT_NAME} ${EXT_CLK} ${EXT_RESET} 50000000 0x83c44000 4K 0]
+#[AXI_DEV_CONNECT ${C2C_PHY} ${AXI_INTERCONNECT_NAME} ${EXT_CLK} ${EXT_RESET} 50000000 0x83c44000 4K 0]
 
 
 
 #================================================================================
 #  Configure and add AXI slaves
 #================================================================================
-source ${apollo_root_path}/src/c2cSlave/AddSlaves.tcl
+source ../configs/${build_name}/autogen/AddSlaves_${build_name}.tcl
 
 #========================================
 #  Finish up
