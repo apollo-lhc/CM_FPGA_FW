57127c69ad7ed452827b0ab7207524e07d6f47ed
Dan Gastler <dgastler@gmail.com>
2021-08-04 16:38:58 -0400

    feature/update_regmap

--- configs/Cornell_rev1_p2_VU7p-1-SM_USP/settings.tcl
+++ configs/Cornell_rev1_p2_VU7p-1-SM_USP/settings.tcl
@@ -6,6 +6,13 @@ set FPGA_part xcvu7p-flvb2104-1-e
 set C2C V_C2C
 set C2C_PHY ${C2C}_PHY
 
+#enable remote device tree entries
+#also set them to be 64bit values
+global REMOTE_C2C
+global REMOTE_C2C_64
+set REMOTE_C2C 1
+set REMOTE_C2C_64 1
+
 set top top
 
 set outputDir ./
