2b7ab30ef0bbe4dad1f397a4daff1f9feec9a3b8
Peter Wittich <wittich@cornell.edu>
2022-07-25 18:22:04 -0400

    update from V_ to F1_ for P1 position to match CM schematic

--- configs/Cornell_rev2_p1_VU13p-1-SM_USP/settings.tcl
+++ configs/Cornell_rev2_p1_VU13p-1-SM_USP/settings.tcl
@@ -3,9 +3,9 @@
 set FPGA_part xcvu13p-flga2577-1-e
 
 ##for c2c
-set C2C V_C2C
+set C2C F1_C2C
 set C2C_PHY ${C2C}_PHY
-set C2CB V_C2CB
+set C2CB F1_C2CB
 set C2CB_PHY ${C2CB}_PHY
 
 #create remote device tree entries, 64 bit
