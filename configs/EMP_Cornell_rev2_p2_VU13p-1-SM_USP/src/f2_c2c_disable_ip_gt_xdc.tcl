dabc10b9a878eab264d0f7a91d07a9fc6b0a314b
Jonathan Fulcher <furryboffin@gmail.com>
2026-04-21 17:30:41 +0200

    c2c_dma_jrf_128_to_64 - Stillt trying to fix placement

--- configs/EMP_Cornell_rev2_p2_VU13p-1-SM_USP/src/f2_c2c_disable_ip_gt_xdc.tcl
+++ configs/EMP_Cornell_rev2_p2_VU13p-1-SM_USP/src/f2_c2c_disable_ip_gt_xdc.tcl
@@ -4,6 +4,10 @@
 # before implementation reads XDC constraints, preventing LOC collisions and
 # illegal COMMON<->CHANNEL pairings being forced by *_gt.xdc.
 
+# NOTE: Some CI log captures only show WARNING/ERROR lines. Emit a single
+# WARNING banner so we can confirm this hook executed.
+puts "WARNING: f2_c2c_disable_ip_gt_xdc.tcl: running (attempting to disable *_gt.xdc if visible as project files)"
+
 set gt_xdc_patterns [list \
 	"*c2cSlave_F2_C2C_PHY_0_gt.xdc" \
 	"*c2cSlave_F2_C2CB_PHY_0_gt.xdc" \
@@ -13,6 +17,7 @@ foreach pat $gt_xdc_patterns {
 	set gt_files [get_files -all -quiet $pat]
 	if {[llength $gt_files] == 0} {
 		puts "WARNING: f2_c2c_disable_ip_gt_xdc.tcl: no GT XDC files matched pattern: $pat"
+		puts "WARNING: f2_c2c_disable_ip_gt_xdc.tcl: (this can happen if the IP reads *_gt.xdc internally and it is not a project file object)"
 		continue
 	}
 
@@ -20,5 +25,5 @@ foreach pat $gt_xdc_patterns {
 		set_property USED_IN_SYNTHESIS 0 $f
 		set_property USED_IN_IMPLEMENTATION 0 $f
 	}
-	puts "INFO: f2_c2c_disable_ip_gt_xdc.tcl: disabled IP GT constraint file(s): $gt_files"
+	puts "WARNING: f2_c2c_disable_ip_gt_xdc.tcl: disabled IP GT constraint file(s): $gt_files"
 }
