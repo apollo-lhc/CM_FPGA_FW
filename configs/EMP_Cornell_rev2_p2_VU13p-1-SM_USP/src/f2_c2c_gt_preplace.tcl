107dab83e6d536302edffa6818820e1f7b968395
Jonathan Fulcher <furryboffin@gmail.com>
2026-04-21 17:28:47 +0200

    c2c_dma_jrf_128_to_64 - Stillt trying to fix placement

--- configs/EMP_Cornell_rev2_p2_VU13p-1-SM_USP/src/f2_c2c_gt_preplace.tcl
+++ configs/EMP_Cornell_rev2_p2_VU13p-1-SM_USP/src/f2_c2c_gt_preplace.tcl
@@ -3,6 +3,10 @@
 # This runs with the design open, after IP-generated constraints have been read,
 # so it reliably overrides hard-LOCs from GT wizard *_gt.xdc.
 
+# NOTE: Some CI log captures only show WARNING/ERROR lines. Emit a single
+# WARNING banner so we can confirm this hook executed.
+puts "WARNING: f2_c2c_gt_preplace.tcl: running pre-place GT LOC enforcement"
+
 proc _set_loc_if_found {ref_name name_glob loc} {
 	set cells [get_cells -hierarchical -quiet -filter [format {REF_NAME == "%s" && NAME =~ "%s"} $ref_name $name_glob]]
 	if {[llength $cells] == 0} {
@@ -14,10 +18,10 @@ proc _set_loc_if_found {ref_name name_glob loc} {
 }
 
 # Primary C2C GT channel (F2_C2C_PHY)
-_set_loc_if_found GTYE4_CHANNEL *c2cSlave_i*F2_C2C_PHY*GTYE4_CHANNEL_PRIM_INST GTYE4_CHANNEL_X1Y0
+_set_loc_if_found GTYE4_CHANNEL *c2cSlave_i*F2_C2C_PHY*gen_channel_container\[24\]*GTYE4_CHANNEL_PRIM_INST GTYE4_CHANNEL_X1Y0
 
 # Secondary C2C GT channel (F2_C2CB_PHY)
-_set_loc_if_found GTYE4_CHANNEL *c2cSlave_i*F2_C2CB_PHY*GTYE4_CHANNEL_PRIM_INST GTYE4_CHANNEL_X1Y1
+_set_loc_if_found GTYE4_CHANNEL *c2cSlave_i*F2_C2CB_PHY*gen_channel_container\[24\]*GTYE4_CHANNEL_PRIM_INST GTYE4_CHANNEL_X1Y1
 
 # Common/QPLL for the quad
 _set_loc_if_found GTYE4_COMMON *c2cSlave_i*F2_C2C_PHY*GTYE4_COMMON_PRIM_INST GTYE4_COMMON_X1Y0
