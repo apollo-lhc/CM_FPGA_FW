9a9abe5a869d188822097d7f4e983a0df903b0a0
Jonathan Fulcher <furryboffin@gmail.com>
2026-04-21 13:25:09 +0200

    c2c_dma_jrf_128_to_64 - Stillt trying to fix placement

--- configs/IT-DTC_p1_VU13p-1/files.tcl
+++ configs/IT-DTC_p1_VU13p-1/files.tcl
@@ -41,6 +41,7 @@ set xdc_files "\
     configs/${build_name}/src/top_pins.xdc \
     configs/${build_name}/src/top_pins_TCDS.xdc \
     configs/${build_name}/src/top_timing.xdc	\
+    configs/${build_name}/src/gt_quad_l_override.xdc \
     configs/${build_name}/it-dtc-fw/src/top_timing.xdc \
     "	    
 
