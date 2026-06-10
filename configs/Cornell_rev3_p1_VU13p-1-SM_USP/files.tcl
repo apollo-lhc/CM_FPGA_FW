638689d4f9f0f66f4d804bcadfebb277ab4f6ec8
Jonathan Fulcher <furryboffin@gmail.com>
2026-06-10 10:06:26 +0200

    c2c_dma_jrf_128_to_64 - clean up IT-DTC removal after rebase

--- configs/Cornell_rev3_p1_VU13p-1-SM_USP/files.tcl
+++ configs/Cornell_rev3_p1_VU13p-1-SM_USP/files.tcl
@@ -39,7 +39,6 @@ set vhdl_files "\
 set xdc_files "\
     configs/${build_name}/src/top_pins.xdc \
     configs/${build_name}/src/top_timing.xdc	\
-    configs/${build_name}/src/gt_quad_l_override.xdc \
     "	    
 
 set xci_files "\
